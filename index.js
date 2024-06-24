86851a0df189a4af3c085d40a6bcab6412751cd0
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-25 00:18:03 +0600

    add configuration

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ const http = require('http');
 
 //app - Object
 const app = {}
+
+//cofiguration
+app.config = {
+    port: 3000
+}
+
+app.createServer = () => {
+
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Listening to port ${app.config.port}`)
+    })
+
+}
+
+app.handleReqRes = (req, res) => {
+    res.end('Hello World');
+}
+
+app.createServer();
