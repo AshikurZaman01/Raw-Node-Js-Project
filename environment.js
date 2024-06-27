ee9c4d41834a82d2c4dc4fc4723ad3b15d7bd7a0
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-06-27 23:49:22 +0600

    finish env

--- environment.js
+++ environment.js
@@ -12,3 +12,9 @@ environment.production = {
     port: 5000,
     envName: 'production',
 }
+
+const currentEnv = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : 'stagging';
+
+const envToExport = typeof (environment[currentEnv]) === 'object' ? environment[currentEnv] : environment.stagging;
+
+module.exports = envToExport;
