645f350399774bdf7329083437035091bd336f46
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-07-02 14:44:48 +0600

    ejs apps

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
-const { log } = require('console');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -12,10 +11,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-
     fs.readdir(`./files`, (err, files) => {
         res.render('index', { files: files });
     });
+})
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+        res.redirect('/');
+    });
+})
+
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, (err, data) => {
+        console.log(data);
+    })
 })
 
 app.listen(port, () => {
