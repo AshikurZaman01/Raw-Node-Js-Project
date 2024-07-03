fa22ac1016a83eadf92067f3135770ea6b2a866f
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-07-04 01:02:34 +0600

    mongoose syntax

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-
 const express = require('express');
+const { all } = require('express/lib/application');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
-const path = require('path');
-const fs = require('fs');
 
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const todoHandler = require('./routerHandler/todoHandler.js');
 
-app.get('/', (req, res) => {
-    fs.readdir(`./files`, (err, files) => {
-        res.render('index', { files: files });
-    });
-})
 
-app.post('/create', (req, res) => {
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
-        res.redirect('/');
-    });
-})
+mongoose
+    .connect('mongodb://localhost:27017/todos',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    .then(() => { console.log('Connected to MongoDB') })
+    .catch((err) => { console.error('Could not connect to MongoDB', err) });
 
-app.get('/file/:filename', (req, res) => {
-    fs.readFile(`./files/${req.params.filename}`, (err, data) => {
-        console.log(data);
-    })
-})
+
+app.use('/todo', todoHandler);
+
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Example app listening on port ${port}`)
 })
